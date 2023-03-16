b9e91fda4bceea2f7a39e1f72259b92c02a57767
Tyson Peterson <tysonpeterson4@gmail.com>
2023-03-16 13:42:26 -0600

    created player stats and images

---
+++ create.js
@@ -0,0 +1,29 @@
+let ballPlayers = [
+  { name:"Yoda", picture:"yoda.webp", iq:8, speed:5, strength:2, skill:8 },
+  { name:"Snoop Dogg", picture:"snoopdogg.jpeg", iq:3, speed:5, strength:5, skill:4 },
+  { name:"Dancing Pyramid", picture:"pyramidman.jpeg", iq:1, speed:3, strength:6, skill:6 },
+  { name:"Bad Bunny", picture:"badbunny.jpeg", iq:0, speed:5, strength:4, skill:6 },
+  { name:"Tucker Carlson", picture:"tuckercarlson.webp", iq:9, speed:3, strength:2, skill:2 },
+  { name:"LEGO Hulk", picture:"legohulk.jpeg", iq:0, speed:3, strength:10, skill:2 },
+  { name:"Ghengis Kahn", picture:"ghenghiskhan.webp", iq:5, speed:6, strength:6, skill:4 },
+  { name:"Larry the Cucumber", picture:"larry.webp", iq:4, speed:2, strength:2, skill:9 },
+  { name:"King Tut", picture:"kingtut.avif", iq:6, speed:3, strength:3, skill:7 },
+  { name:"Albert Einstein", picture:"alberteinstein.jpeg", iq:100, speed:1, strength:1, skill:3 },
+  { name:"Burton Guster", picture:"burtonguster.webp", iq:8, speed:4, strength:6, skill:4 },
+  { name:"Teddy Roosevelt", picture:"teddyroosevelt.jpeg", iq:8, speed:4, strength:4, skill:7 },
+  { name:"Flynn Rider", picture:"flynnrider.jpeg", iq:2, speed:7, strength:3, skill:6 },
+  { name:"John Wick", picture:"johnwick.jpeg", iq:6, speed:3, strength:3, skill:8 },
+  { name:"Enderman", picture:"enderman", iq:0, speed:7, strength:1, skill:7 },
+  { name:"Draco Malfoy (Kid)", picture:"dracomalfoy.jpeg", iq:8, speed:5, strength:1, skill:8 },
+  { name:"Waluigi", picture:"waluigi.jpeg", iq:1, speed:1, strength:1, skill:10 },
+  { name:"Light Yagami", picture:"lightyagami.webp", iq:10, speed:2, strength:2, skill:5 },
+  { name:"Jotaro Kujo", picture:"jotarokujo.jpeg", iq:5, speed:7, strength:7, skill:7 },
+  { name:"Michael Jackson", picture:"michaeljackson.png", iq:1, speed:3, strength:3, skill:5 },
+  { name:"Arthur", picture:"arthur.jpeg", iq:7, speed:3, strength:6, skill:2 },
+  { name:"Bruce Lee", picture:"brucelee.avif", iq:4, speed:8, strength:5, skill:8 },
+  { name:"Rock Lee", picture:"rocklee.jpeg", iq:3, speed:9, strength:6, skill:2 },
+  { name:"Ash Ketchum", picture:"ashketchum.jpeg", iq:5, speed:5, strength:1, skill:7 },
+  { name:"Jake the Dog", picture:"jakethedog.jpeg", iq:2, speed:2, strength:2, skill:2 }
+]
+
+
